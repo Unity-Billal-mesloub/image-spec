0d57555d7545281ba53c8872835a7c8cf655292e
Billal Mesloub <mesloubi2021@gmail.com>
2026-08-17 04:53:44 +0100

    Remove copyright and license comments from chainid.go
    Removed copyright notice and license comments from the file.
    
    Signed-off-by: Billal Mesloub <mesloubi2021@gmail.com>

--- identity/chainid.go
+++ identity/chainid.go
@@ -1,27 +1,7 @@
-// Copyright the Open Container Initiative Contributors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-// Package identity provides implementations of subtle calculations pertaining
-// to image and layer identity.  The primary item present here is the ChainID
-// calculation used in identifying the result of subsequent layer applications.
-//
-// Helpers are also provided here to ease transition to the
-// github.com/opencontainers/go-digest package, but that package may be used
-// directly.
 package identity
 
-import "github.com/opencontainers/go-digest"
+import "github.com/Unity-Billal-mesloub/go-digest"
 
 // ChainID takes a slice of digests and returns the ChainID corresponding to
 // the last entry. Typically, these are a list of layer DiffIDs, with the
